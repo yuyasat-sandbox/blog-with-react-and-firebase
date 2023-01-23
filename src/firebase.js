266451cb72cacc8bbbe79b91fbf376478357a874
yuyasat <takiyour@gmail.com>
2023-01-23 10:33:27 +0900

    introduce firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyADzbcrl-xFVs4Vq3smXVZSSbRG4sH4aSg",
+    authDomain: "blog-with-react-and-fire-ba0bc.firebaseapp.com",
+    projectId: "blog-with-react-and-fire-ba0bc",
+    storageBucket: "blog-with-react-and-fire-ba0bc.appspot.com",
+    messagingSenderId: "867993900814",
+    appId: "1:867993900814:web:a941665eb19f963f99295f"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+export { auth, provider, db };
