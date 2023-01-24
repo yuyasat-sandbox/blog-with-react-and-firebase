fb33cd133bfd50700a3635adb49e5b291bc2ad7a
yuyasat <takiyour@gmail.com>
2023-01-24 21:52:13 +0900

    Navbar icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faFilePen, faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
     return (
         <nav>
-            <Link to="/">ホーム</Link>
-            <Link to="/createpost">記事投稿</Link>
-            <Link to="/login">ログイン</Link>
+            <Link to="/"><FontAwesomeIcon icon={faHouse} />ホーム</Link>
+            <Link to="/createpost"><FontAwesomeIcon icon={faFilePen} />記事投稿</Link>
+            <Link to="/login"><FontAwesomeIcon icon={faArrowRightToBracket} />ログイン</Link>
         </nav>
     )
 }
