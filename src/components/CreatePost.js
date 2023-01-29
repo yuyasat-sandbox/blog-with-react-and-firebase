2e2404694eebfe7417a1ac98c6245065f18ff1b9
yuyasat <takiyour@gmail.com>
2023-01-29 22:38:14 +0900

    create post

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,20 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./CreatePost.css";
+import { collection, addDoc } from "firebase/firestore";
+import { auth, db } from "../firebase";
+import { useNavigate } from 'react-router-dom';
 
 const CreatePost = () => {
+  const [title, setTitle] = useState();
+  const [postText, setPostText] = useState();
+  const navigate = useNavigate();
+
+  const createPost = async () => {
+    console.log(auth.currentUser);
+    // Add a new document with a generated id.
+    const docRef = await addDoc(collection(db, "posts"), {
+      title: title,
+      postsText: postText,
+      auther: {
+        username: auth.currentUser.displayName,
+        uid: auth.currentUser.uid,
+      }
+    });
+    navigate("/");
+  }
+
   return (
     <div className="createPostPage">
       <div className="postContainer">
         <h1>記事を投稿する</h1>
         <div className="inputPost">
           <div>タイトル</div>
-          <input type="text" placeholder='タイトルを記入' />
+          <input
+            type="text"
+            placeholder='タイトルを記入'
+            onChange={(e) => {
+              setTitle(e.target.value)
+            }} />
         </div>
         <div className="inputPost">
           <div>投稿</div>
-          <textarea type="text" placeholder='投稿内容を記入' />
+          <textarea
+            type="text"
+            placeholder='投稿内容を記入'
+            onChange={(e) => {
+              setPostText(e.target.value)
+            }} />
         </div>
-        <button className="postButton">投稿する</button>
+        <button className="postButton" onClick={createPost}>投稿する</button>
       </div>
     </div>
   )
