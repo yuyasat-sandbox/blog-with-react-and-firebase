693d5c3c72100746baf46a545c495d00f767a393
yuyasat <takiyour@gmail.com>
2023-01-30 15:58:40 +0900

    Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='homePage'>
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          あいうえおかきくけこさしすせそxxxxxxxxxxxxxxxxxxxxxxxxxxxxddddddddddddddddddddddddddddddddddddddddsssssssssssssssssssssssss
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>@yuyasat</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
